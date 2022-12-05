2b1814cd8a046d981d9fbbfabfd2410e19ec883f
Jorge Aspiolea <aspiolea28@gmail.com>
2022-12-05 11:29:58 -0500

    Add new branch changes

--- Archivo/documents/models.py
+++ Archivo/documents/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.urls import reverse
 
+# class Document(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=200)
+#     year=models.IntegerField(default=0)
+#     document_type=models.CharField(max_length=100, default="")
+#     body = models.TextField()
+
+#     def __str__(self):
+#         return self.title
+
+#     def get_absolute_url(self):
+#         return reverse("document.html", args=[str(self.id)])
+    
 class Document(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    year=models.IntegerField(default=0)
-    document_type=models.CharField(max_length=100, default="")
-    body = models.TextField()
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse("document.html", args=[str(self.id)])
+    xml_data = models.TextField()
     
